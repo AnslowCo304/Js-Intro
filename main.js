95de639274a2f5a5f046f0a25a212b3b2cf20aa7
AnslowCo304 <AnslowCo304@northwested.org>
2026-03-11 10:16:41 -0400

    Oof

--- main.js
+++ main.js
@@ -2,16 +2,16 @@
 // 10 MAR 2026
 // Variable Declaration & Assignment 
 
-const firstName = "Connor"
-const lastName = "Anslow"
-const eyeColor = "Blue-Gray"
-const sendingSchool = "Suttons Bay"
-const program = "Web And App Development"
-const grade = "11"
+const firstName = "Connor";
+const lastName = "Anslow";
+const eyes = "Blue Gray";
+const sendingSchool = "Suttons Bay";
+const program = "Web & App Development";
+const grade = "11";
 
 console.log(firstName);
 console.log(lastName);
-console.log(eyeColor);
+console.log(eyes);
 console.log(sendingSchool);
 console.log(program);
 console.log(grade);
