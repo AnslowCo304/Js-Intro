7ef8c6dcf1dd2476fd0cfbbfff9b8b6d55044f41
AnslowCo304 <AnslowCo304@northwested.org>
2026-03-11 10:34:51 -0400

    Create report.js

---
+++ report.js
@@ -0,0 +1,19 @@
+// Connor Anslow
+// 11 MAR 2026
+// JavaScript Report
+
+const firstName = "Bindapeeple";
+const lastName = "Knimbeebalo";
+const homeSchool = "Pleasantville High";
+let quiz1 = "72";
+let quiz2 = "90";
+let quiz3 = "36";
+let averageScore = (quiz1 + quiz2 + quiz3) / 3;
+
+console.log(firstName);
+console.log(lastName);
+console.log(homeSchool);
+console.log(quiz1);
+console.log(quiz2);
+console.log(quiz3);
+console.log(averageScore);
