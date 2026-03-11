665670deeb954f0457f77cc6883a41466775f83f
AnslowCo304 <AnslowCo304@northwested.org>
2026-03-10 20:19:20 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+// Connor Anslow
+// 10 MAR 2026
+// Variable Declaration & Assignment 
+
+const firstName = "Connor"
+const lastName = "Anslow"
+const eyeColor = "Blue-Gray"
+const sendingSchool = "Suttons Bay"
+const program = "Web And App Development"
+const grade = "11"
+
+console.log(firstName);
+console.log(lastName);
+console.log(eyeColor);
+console.log(sendingSchool);
+console.log(program);
+console.log(grade);
